276f44e277325ae79d06f37f82a452e93386ae14
alexander.prognimak <alexander.prognimak@seznam.cz>
2017-06-10 08:44:44 +0100

    sceleton of hello object

--- hello_world/hello.cpp
+++ hello_world/hello.cpp
@@ -1,4 +1,30 @@
 #include <iostream>
+
+using namespace std;
+
+struct Hello{
+ int n; //number of times to greet
+ string  greeting; // Hello World
+
+ void initialize(int num, char* greet)
+ {
+   n=num;
+   greeting=greet;
+ }
+
+  void create()
+  {
+    n=0;
+    greeting = "Hello World";
+  }
+
+  void destroy()
+  {
+    n=0;
+    greeting = "";
+  }
+};
+
  int main()
  {
   std::cout << "Hello World bitches v0.5!";
