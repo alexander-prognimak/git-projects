492fc5c5e52bf7f6c31ff0d4980c7dccbd8b552d
alexander.prognimak <alexander.prognimak@seznam.cz>
2017-06-18 00:09:22 +0100

    setting 2ndary functions

--- hacker_rank_data_structures/dynamic_array/dynamic_array.cpp
+++ hacker_rank_data_structures/dynamic_array/dynamic_array.cpp
@@ -3,15 +3,58 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+
 using namespace std;
 
-class SeqList{
-  std::vector <int> sequences;
+//NB get rid of the below
+class elements{
+  vector <int> values;
+};
 
+class SeqList{
+public:
+  SeqList(int N);
+  ElementAppend(int position, int value);
+  int LastAnswer = 0;
+  PrintSeqList();
+private:
+  int size_N;
+  vector<elements> SeqListArray;
+};
+
+void PrintSeqList()
+{
+  int n=SeqListArray[position].elements.size();
+  for (int i=0; i<n; ++i)
+  {
+    
+  }
+}
+
+void ElementAppend(int position, int value)
+{
+  int new_node_size = SeqListArray[position].elements.size()+1;
+  SeqListArray[position].elements.reserve(new_node_size);
+  SeqListArray[position].elements[new_node_size] = value;
 }
 
 
+void SeqList::SeqList(int N)
+{
+  SeqListArray.reserve(N);
+}
+
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    // declarations
+    int N, Q;
+
+    // Reading N(number of sequences) and Q(number of queries)
+    cin >> N >> Q;
+
+    //Looping through queries
+
+    // Returning the result
+
     return 0;
 }
