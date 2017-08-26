7b6ca2b2e1d4820369d836805c174bc9abd95294
alexander.prognimak <alexander.prognimak@seznam.cz>
2017-08-26 16:22:50 +0100

    linked lists 2.1 and 2.3

--- hello_world/cracking/list_1.cpp
+++ hello_world/cracking/list_1.cpp
@@ -15,25 +15,27 @@ public:
     value=n;
     next_node = NULL;
   };
+  ~Node()
+  {
+//      std::cout << "Destructor Node:"<< value;
+      delete(next_node);
+  }
 };
 
 class Tree{
 public:
   Node* head;
   int size;
-  Tree(Node& new_node)
+  Tree(int x)
   {
-    head = &new_node;
+    head = new Node(x);
     size = 1;
   }
 
   ~Tree()
   {
-    if (this->size > 1)
-      while(this->size > 1)
-        remove_node(1);
+//    std::cout << "Destrcutor:"<< this->size <<"!";
     delete(this->head);
-    this->head = NULL;
   }
 
   void add_node(int n)
@@ -48,79 +50,118 @@ public:
       current_node->next_node = new_node;
     }
     else
+    {
+      std::cout << "AHTUNG NEW HEAD created\n";
       this->head = new_node;
-
+    }
     ++this->size;
   }
 
-  Node* scroll_list(int index, int print = 0)
+
+  void print_tree()
   {
-    //std::cout<<"DEBUG " << index <<"|" << this->size;
-    assert(index<=this->size);
-    assert(index>0);
     int counter(0);
 
     Node* current_node = this->head;
     // Scroll through the list
-    while (current_node != NULL && counter <= index-1) // we start counting from 0
+    while (current_node != NULL) // we start counting from 0
     {
-      if (print)
-        std::cout << "node[" << counter << "]=" << current_node->value <<";\n";
-
+      std::cout << "node[" << counter << "]=" << current_node->value <<";\n";
       current_node = current_node->next_node;
       ++counter;
     }
-
-    return current_node;
   }
 
-  int  remove_node(int index)
+  void remove_node(int index)
   {
-    if (index>this->size || index<=0) // head node deleted with tree
-    {
-      std::cout << "Can't remove index " << index << "- less thatn size of linked list" << this->size<<std::endl;
-      return 0;
+    assert(index<=this->size);
+    assert(index>=0);
+    int counter(0);
+
+    if (index ==0)
+    { // Head of list
+      Node* current_node = this->head;
+      this->head = (this->head)->next_node;
+      current_node->next_node = NULL;
+      delete(current_node);
     }
     else
     {
-        Node* node_to_delete = scroll_list(index-1);
-        // we assume it is not NULL because other size >0
+      Node* current_node = this->head;
+      Node* prev_node = this-> head;
+      // Scroll through the list
+      while (current_node != NULL && counter <= index-1) // we start counting from 0
+      {
+//        if (print)
+//          std::cout << "node[" << counter << "]=" << current_node->value <<";\n";
+        prev_node = current_node;
+        current_node = current_node->next_node;
+        ++counter;
+      }
 
-        if ((Node*)(node_to_delete->next_node)->next_node == NULL) // last node
-        {
-          delete(node_to_delete->next_node);
-          node_to_delete->next_node = NULL;
-          --this->size;
-        }
-        else   // mid node
-        {
-          Node* next_to_delete = node_to_delete->next_node;
-          node_to_delete->next_node = NULL;
-          while(next_to_delete!=NULL)
-          {
-            node_to_delete = next_to_delete;
-            next_to_delete= next_to_delete->next_node;
-            delete(node_to_delete);
-            --this->size;
-          }
-        }
+      // Deleting the node
+      if (current_node->next_node == NULL)
+      { //last node
+        prev_node->next_node = NULL;
+        delete(current_node);
+      }
+      else
+      { // mid node
+          prev_node->next_node = current_node->next_node;
+          current_node->next_node = NULL;
+          delete(current_node);
+      }
     }
-
-    return 1;
   }
-
 };
 
+
 /* Task 1 Remove duplicates from linked list*/
+void tree_remove_duplicates(Tree& tree_to_clean)
+{
+  int current_index(0);
+  Node* current_node = tree_to_clean.head;
+  int current_value(0);
+
+  while (current_node->next_node != NULL)
+  {
+   current_value = current_node->value;
+   Node* iterator = current_node->next_node;
+   int iterator_index = current_index+1;
+
+   do
+   {
+     if (iterator->value == current_value)
+      {
+      std::cout << "removing " << iterator_index << " node\n";
+      if (iterator->next_node != NULL)
+        iterator = iterator->next_node;
+      tree_to_clean.remove_node(iterator_index);
+      }
+    else
+    {
+      if (iterator->next_node != NULL)
+        iterator = iterator->next_node;
+      ++iterator_index;
+    }
+  }while  (iterator->next_node != NULL);
+
+    if (current_node->next_node !=NULL)
+      current_node = current_node->next_node;
+    std::cout << "Cleaning end "<< current_index << "+1\n";
+    ++current_index;
+  }
+}
+
+/* TODO there is small bug above with test case i/6 - FIX if have time */
+
+
 int main()
 {
-  Node head_node(0);
-  Tree test_tree(head_node);
-
+  Tree test_tree(0);
   for (int i=0; i< 10; ++i)
-    test_tree.add_node(i);
-
-  test_tree.scroll_list(test_tree.size, 1);
-
-
+    test_tree.add_node(i/4);
+  test_tree.print_tree();
+  tree_remove_duplicates(test_tree);
+  test_tree.print_tree();
 }
